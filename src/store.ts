5fa9921027c96e29068ca7264b2ebf8f45087331
Magdalena Buzikiewicz <buzu@op.pl>
2021-06-05 14:35:20 +0200

    Created store and wrapped App component with store Provider. Added redux-devtools-extension - devToolsEnhancer to enable view in dev tools.

---
+++ src/store.ts
@@ -0,0 +1,8 @@
+import { rootReducer } from "./rootReducer"
+import { devToolsEnhancer } from "redux-devtools-extension"
+import { createStore } from "redux"
+
+
+
+
+export const store = createStore(rootReducer, devToolsEnhancer({}))
