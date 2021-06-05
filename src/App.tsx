a38f664fe5709501e09654d102c67d20c9c9c35a
Magdalena Buzikiewicz <buzu@op.pl>
2021-06-05 15:14:56 +0200

    Handling Canvas events.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React, { useRef } from 'react';
 
-
+// pass null as the default value to the useRef hook, otherwise-type error stating that the ref prop of the canvas element does not accept undefined
 export const App = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  return <canvas ref={canvasRef} />
+
+  const startDrawing = () => { }
+  const endDrawing = () => { }
+  const draw = () => { }
+
+  return (
+    <canvas
+      ref={canvasRef}
+      onMouseDown={startDrawing}
+      onMouseUp={endDrawing}
+      onMouseOut={endDrawing}
+      onMouseMove={draw}
+    />
+  )
 }
