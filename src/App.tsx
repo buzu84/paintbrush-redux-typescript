97a8d99ede8239dbd545fbdd19c2c55c3d01f6db
Magdalena Buzikiewicz <buzu@op.pl>
2021-06-05 16:09:03 +0200

    Use useSelector to retrieve store data(current stroke).

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef } from 'react'
+import { useSelector } from "react-redux"
+import { currentStrokeSelector } from './selectors'
 
 // pass null as the default value to the useRef hook, otherwise-type error stating that the ref prop of the canvas element does not accept undefined
 export const App = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const currentStroke = useSelector(currentStrokeSelector)
 
   const startDrawing = () => { }
   const endDrawing = () => { }
