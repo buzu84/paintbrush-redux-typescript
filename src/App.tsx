322fdb52d4082be8da290dc6d20fb792ab1f3a73
Magdalena Buzikiewicz <buzu@op.pl>
2021-06-05 16:28:44 +0200

    Dispatch actions upon startDrawing and draw events.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import React, { useRef } from 'react'
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from 'react-redux'
 import { currentStrokeSelector } from './selectors'
+import { beginStroke, endStroke, updateStroke } from './actions'
 
 // pass null as the default value to the useRef hook, otherwise-type error stating that the ref prop of the canvas element does not accept undefined
 export const App = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const currentStroke = useSelector(currentStrokeSelector)
 
-  const startDrawing = () => { }
+  const currentStroke = useSelector(currentStrokeSelector)
+  // if there is at least one point in the current stroke points array-drawing has started
+  // converting the current stroke points array length to a boolean
+  const isDrawing = !!currentStroke.points.length
+
+  const dispatch = useDispatch()
+
+  // mouse press event handler-make it dispatch the BEGIN_STROKE action.
+  // mouse coordinates from the offsetX and offsetY fields of the nativeEvent and pass them with the action
+  const startDrawing = ({
+    nativeEvent
+  }: React.MouseEvent<HTMLCanvasElement>) => {
+    const { offsetX, offsetY } = nativeEvent
+    dispatch(beginStroke(offsetX, offsetY))
+  }
+
   const endDrawing = () => { }
-  const draw = () => { }
+
+  // move event in the draw handler. isDrawing flag to check that the mouse is pressed
+  const draw = ({
+    nativeEvent
+  }: React.MouseEvent<HTMLCanvasElement>
+  ) => {
+    if (!isDrawing) {
+      return
+    }
+    const { offsetX, offsetY } = nativeEvent
+    dispatch(updateStroke(offsetX, offsetY))
+  }
 
   return (
     <canvas
