a36847d2c56eefd4b781f39b5bfb2ccb14e98fa7
Magdalena Buzikiewicz <buzu@op.pl>
2021-06-05 15:39:26 +0200

    Action creators have been added.

--- src/actions.ts
+++ src/actions.ts
@@ -20,3 +20,13 @@ export type Action =
 export const BEGIN_STROKE = "BEGIN_STROKE"
 export const UPDATE_STROKE = "UPDATE_STROKE"
 export const END_STROKE = "END_STROKE"
+
+export const beginStroke = (x: number, y: number) => {
+  return { type: BEGIN_STROKE, payload: { x, y } }
+}
+export const updateStroke = (x: number, y: number) => {
+  return { type: UPDATE_STROKE, payload: { x, y } }
+}
+export const endStroke = () => {
+  return { type: END_STROKE }
+}
