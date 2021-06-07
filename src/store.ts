b28f813702288ae91b44352df4f0b45fa963efe2
Magdalena Buzikiewicz <buzu@op.pl>
2021-06-07 18:22:45 +0200

    Dummy backend-express, cors, lowdb, concurrently, ts-node based.

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import logger from "redux-logger"
-import { configureStore, getDefaultMiddleware, combineReducers } from "@reduxjs/toolkit"
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
 import historyIndex from './modules/historyIndex/slice'
 import { currentStroke } from './modules/currentStroke/slice'
 import strokes from './modules/strokes/slice'
